2320909d6b6b3f7e1d7afc80fbf45e5bc99d1c61
sicchio99 <giovannisicchio@gmail.com>
2024-06-07 18:46:26 +0200

    wall_robot

--- New New Project/wall_robot.py
+++ New New Project/wall_robot.py
@@ -24,6 +24,27 @@ def get_robot_orientation():
     return orientation[2]  # Restituisce l'angolo yaw
 
 
+def normalize_angle(angle):
+    """
+    Normalizza l'angolo in modo che sia compreso tra -pi e pi.
+    """
+    while angle > math.pi:
+        angle -= 2 * math.pi
+    while angle < -math.pi:
+        angle += 2 * math.pi
+    print("Normalizzato", angle)
+    return angle
+
+
+def angular_difference(angle1, angle2):
+    """
+    Calcola la differenza tra due angoli, tenendo conto del wrapping.
+    """
+    print("target", angle1, "current", angle2)
+    diff = angle1 - angle2
+    return normalize_angle(diff)
+
+
 def go_straight(left_dist, front_dist):
     error = front_dist - left_dist
     adjustment = kp * error
@@ -50,11 +71,11 @@ def turn_randomly(front, left, right):
     direction = random.choice(options)
 
     if direction == "Right":
-        turn_right()
         print("Right")
+        turn_right()
     elif direction == "Left":
-        # turn_left()
         print("Left")
+        turn_left()
     elif direction == "Front":
         print("Front")
         normal_go_straight()
@@ -67,37 +88,106 @@ def turn_right():
     # current_angle = get_robot_orientation()
     # print("current", current_angle)
     # target_angle = current_angle + math.pi / 2
-    target_angle = math.pi / 2
+    # target_angle = math.pi / 2
+    target_angle = find_target_angle("right")
     print("target", target_angle)
-    set_robot_orientation(target_angle)
+    set_robot_orientation(target_angle, "right")
     normal_go_straight()
     time.sleep(1.5)
 
 
 def turn_left():
-    target_angle = - math.pi / 2
+    # target_angle = math.pi / 2
+    target_angle = find_target_angle("left")
     print("target", target_angle)
-    set_robot_orientation(target_angle)
+    set_robot_orientation(target_angle, "left")
     normal_go_straight()
     time.sleep(1.5)
 
 
-def set_robot_orientation(target_angle):
+def set_robot_orientation(target_angle, direction):
     current_angle = get_robot_orientation()
     # print("current", current_angle)
     # print("diff", abs(target_angle - current_angle))
-    while abs(target_angle - abs(current_angle)) > angle_tolerance:
+    # print("abs", abs(target_angle))
+    diff = abs(abs(target_angle) - abs(current_angle))
+    # diff = angular_difference(target_angle, current_angle)
+    # print("current", current_angle)
+    print("diff", diff)
+    while abs(diff) > angle_tolerance:
+        # print("abs", abs(target_angle))
         print("current", current_angle)
-        print("diff", abs(target_angle - abs(current_angle)))
-        if target_angle > current_angle:
-            set_speeds(turn_speed, -turn_speed)
-        else:
-            set_speeds(-turn_speed, turn_speed)
+        print("diff", diff)
+        # if target_angle > current_angle:
+        if direction == 'right' or direction == 'front':
+            if diff > 0.8:
+                print("Rotazione normale")
+                set_speeds(turn_speed, -turn_speed)
+            elif 0.3 < diff < 0.8:
+                print("Rotazione più lenta")
+                set_speeds(slow_turn_speed, -slow_turn_speed)
+            else:
+                print("Rotazione molto lenta")
+                set_speeds(more_slow_turn_speed, -more_slow_turn_speed)
+        elif direction == 'left':
+            if diff > 0.8:
+                print("Rotazione normale")
+                set_speeds(-turn_speed, turn_speed)
+            elif 0.3 < diff < 0.8:
+                print("Rotazione più lenta")
+                set_speeds(-slow_turn_speed, slow_turn_speed)
+            else:
+                print("Rotazione molto lenta")
+                set_speeds(-more_slow_turn_speed, more_slow_turn_speed)
         current_angle = get_robot_orientation()
-        time.sleep(0.01)  # Piccola pausa per evitare un loop troppo veloce
+        diff = abs(abs(target_angle) - abs(current_angle))
+        # diff = angular_difference(target_angle, current_angle)
+        # time.sleep(0.01)  # Piccola pausa per evitare un loop troppo veloce
 
     # Ferma il robot dopo aver raggiunto l'angolo desiderato
-    set_speeds(0, 0)
+    # set_speeds(0, 0)
+
+
+def find_target_angle(direction):
+    current_angle = get_robot_orientation()
+    print("CURRENT:", current_angle)
+    if direction == 'right':
+        if -0.3 < current_angle < 0.3:
+            return math.pi / 2
+        elif -1.8 < current_angle < -1.2:
+            return math.pi
+        elif current_angle < -2.8 or current_angle > 2.8:
+            return - math.pi
+        elif 1.2 < current_angle < 1.8:
+            return 0
+    if direction == 'left':
+        if -0.3 < current_angle < 0.3:
+            return - math.pi / 2
+        elif -1.8 < current_angle < -1.2:
+            return 0
+        elif current_angle < -2.8 or current_angle > 2.8:
+            return math.pi
+        elif 1.2 < current_angle < 1.8:
+            return - math.pi
+
+
+def go_back():
+    current_angle = get_robot_orientation()
+    target_angle_back = 0
+    if -0.3 < current_angle < 0.3:
+        target_angle_back = math.pi
+    elif -1.8 < current_angle < -1.2:
+        target_angle_back = - math.pi / 2
+    elif current_angle < -2.8 or current_angle > 2.8:
+        target_angle_back = 0
+    elif 1.2 < current_angle < 1.8:
+        target_angle_back = math.pi/2
+    print("target", target_angle_back)
+    set_robot_orientation(target_angle_back, "front")
+    normal_go_straight()
+    time.sleep(1.5)
+
+
 
 
 if __name__ == "__main__":
@@ -114,9 +204,11 @@ if __name__ == "__main__":
 
     base_speed = 2.0
     turn_speed = 0.3
+    slow_turn_speed = 0.2
+    more_slow_turn_speed = 0.1
     MIN_DISTANCE = 0.4
     kp = 1.0
-    angle_tolerance = 0.01  # Tolleranza per considerare l'angolo raggiunto
+    angle_tolerance = 0.02  # Tolleranza per considerare l'angolo raggiunto
 
     sim.startSimulation()
     set_speeds(base_speed, base_speed)
@@ -130,18 +222,24 @@ if __name__ == "__main__":
             front_dist = get_distance(front)
             left_dist = get_distance(left)
             right_dist = get_distance(right)
-            print("left", left_dist)
-            print("right", right_dist)
+            # print("left", left_dist)
+            # print("right", right_dist)
             front_free = is_free(front)
             left_free = is_free(left)
             right_free = is_free(right)
-            print("front", front_free, "left", left_free, "right", right_free)
+            # print("front", front_free, "left", left_free, "right", right_free)
 
             if front_free:
                 if not left_free and not right_free:
                     # go_straight(left_dist, right_dist)
                     normal_go_straight()
                 else:
+                    turn_randomly(front_free, left_free, right_free)
+            else:
+                if not left_free and not right_free:
+                    go_back()
+                else:
+                    print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
                     turn_randomly(front_free, left_free, right_free)
 
     finally:
