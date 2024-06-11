febaceb5e7f8382be6e31aa1441ba35ec712229f
fab10dl6 <fabiodilazzaro.fdl@gmail.com>
2024-06-11 12:52:32 +0200

    coordinate

--- New New Project/test_coordinate.py
+++ New New Project/test_coordinate.py
@@ -19,30 +19,26 @@ def is_free(sensor):
     return dist == 0 or dist > MIN_DISTANCE
 
 
+def get_free():
+    sens_left = sim.getObjectHandle("/ultrasonicSensor[0]")
+    sens_front = sim.getObjectHandle("/ultrasonicSensor[4]")
+    sens_right = sim.getObjectHandle("/ultrasonicSensor[7]")
+    sens_front_free = is_free(sens_front)
+    sens_left_free = is_free(sens_left)
+    sens_right_free = is_free(sens_right)
+    return sens_front_free, sens_left_free, sens_right_free
+
+
 def get_robot_orientation():
     orientation = sim.getObjectOrientation(robot_handle, -1)
     return orientation[2]  # Restituisce l'angolo yaw
 
 
 def normalize_angle(angle):
-    """
-    Normalizza l'angolo in modo che sia compreso tra -pi e pi.
-    """
-    while angle > math.pi:
-        angle -= 2 * math.pi
-    while angle < -math.pi:
-        angle += 2 * math.pi
-    print("Normalizzato", angle)
-    return angle
-
-
-def angular_difference(angle1, angle2):
-    """
-    Calcola la differenza tra due angoli, tenendo conto del wrapping.
-    """
-    print("target", angle1, "current", angle2)
-    diff = angle1 - angle2
-    return normalize_angle(diff)
+    normalized_angle = angle % (2 * math.pi)
+    if normalized_angle >= math.pi:
+        normalized_angle -= 2 * math.pi
+    return normalized_angle
 
 
 def go_straight(left_dist, front_dist):
@@ -57,17 +53,19 @@ def normal_go_straight():
     set_speeds(base_speed, base_speed)
 
 
-def turn_randomly(front, left, right):
+def turn_randomly():
     time.sleep(1.9)
     set_speeds(0, 0)
+    control_front, control_left, control_right = get_free()
 
     options = []
-    if front:
+    if control_front:
         options.append("Front")
-    if left:
+    if control_left:
         options.append("Left")
-    if right:
+    if control_right:
         options.append("Right")
+    print("Opzioni:", str(options))
     direction = random.choice(options)
 
     if direction == "Right":
@@ -87,6 +85,7 @@ def turn_right():
     print("target", target_angle)
     set_robot_orientation(target_angle, "right")
     normal_go_straight()
+    coordinates.set_direction("right")
     time.sleep(1.5)
 
 
@@ -95,58 +94,60 @@ def turn_left():
     print("target", target_angle)
     set_robot_orientation(target_angle, "left")
     normal_go_straight()
+    coordinates.set_direction("left")
     time.sleep(1.5)
 
 
 def set_robot_orientation(target_angle, direction):
-    current_angle = get_robot_orientation()
+    actual_angle = get_robot_orientation()
+    current_angle = normalize_angle(actual_angle)
+    print("current", current_angle)
     diff = abs(abs(target_angle) - abs(current_angle))
-
-    while abs(diff) > angle_tolerance:
+    while diff > angle_tolerance:
         if direction == 'right' or direction == 'front':
             if diff > 0.8:
                 set_speeds(turn_speed, -turn_speed)
             elif 0.3 < diff < 0.8:
                 set_speeds(slow_turn_speed, -slow_turn_speed)
             else:
-                set_speeds(more_slow_turn_speed, -more_slow_turn_speed)
+                set_speeds(slower_turn_speed, -slower_turn_speed)
         elif direction == 'left':
             if diff > 0.8:
                 set_speeds(-turn_speed, turn_speed)
             elif 0.3 < diff < 0.8:
                 set_speeds(-slow_turn_speed, slow_turn_speed)
             else:
-                set_speeds(-more_slow_turn_speed, more_slow_turn_speed)
+                set_speeds(-slower_turn_speed, slower_turn_speed)
         current_angle = get_robot_orientation()
         diff = abs(abs(target_angle) - abs(current_angle))
 
 
 def find_target_angle(direction):
-    current_angle = get_robot_orientation()
+    actual_angle = get_robot_orientation()
+    current_angle = normalize_angle(actual_angle)
     if direction == 'right':
         if -0.3 < current_angle < 0.3:
             return math.pi / 2
         elif -1.8 < current_angle < -1.2:
             return math.pi
         elif current_angle < -2.8 or current_angle > 2.8:
-            return - math.pi
+            return - math.pi / 2
         elif 1.2 < current_angle < 1.8:
             return 0
-    elif direction == 'left':
+    if direction == 'left':
         if -0.3 < current_angle < 0.3:
             return - math.pi / 2
         elif -1.8 < current_angle < -1.2:
             return 0
         elif current_angle < -2.8 or current_angle > 2.8:
-            return math.pi
+            return math.pi / 2
         elif 1.2 < current_angle < 1.8:
             return - math.pi
-    else:
-        print("Errore inaspettato")
 
 
 def go_back():
-    current_angle = get_robot_orientation()
+    actual_angle = get_robot_orientation()
+    current_angle = normalize_angle(actual_angle)
     target_angle_back = 0
     if -0.3 < current_angle < 0.3:
         target_angle_back = math.pi
@@ -158,7 +159,90 @@ def go_back():
         target_angle_back = math.pi/2
     set_robot_orientation(target_angle_back, "front")
     normal_go_straight()
+    coordinates.set_direction("back")
     time.sleep(1.5)
+
+
+crossroads = []
+
+
+class CoordinatesDS:
+
+    _x: int
+    _y: int
+    _prev: str
+    _direction: str
+    _id: int
+
+    def __init__(self, direction):
+        self._x = 0
+        self._y = 0
+        self._direction = direction
+        self._id = 0
+
+    def set_direction(self, direction):
+        tmp = self._direction
+        self._direction = direction
+        self._prev = tmp
+
+    def save_crossroad(self):
+        crossroads.append({self.id, (self._x, self._y)})
+        self.id += 1
+
+    def moving(self):
+        angle = math.degrees(normalize_angle(get_robot_orientation()))
+        norm_45 = normalize_angle(45)
+        norm_135 = normalize_angle(135)
+
+        if -norm_45 <= angle < norm_45:
+            self._prev = "front"
+        elif norm_45 <= angle < norm_135:
+            self._prev = "right"
+        elif -norm_135 <= angle < -norm_45:
+            self._prev = "left"
+        else:
+            self._prev = "back"
+
+        if self._prev == "front":
+            match self._direction:
+                case "front":
+                    self._y += 1
+                case "right":
+                    self._x += 1
+                case "back":
+                    self._y -= 1
+                case "left":
+                    self._x -= 1
+        elif self._prev == "right":
+            match self._direction:
+                case "front":
+                    self._x += 1
+                case "right":
+                    self._y -= 1
+                case "back":
+                    self._x -= 1
+                case "left":
+                    self._y += 1
+        elif self._prev == "back":
+            match self._direction:
+                case "front":
+                    self._y -= 1
+                case "right":
+                    self._x -= 1
+                case "left":
+                    self._x += 1
+                case "back":
+                    self._y += 1
+        elif self._prev == "left":
+            match self._direction:
+                case "front":
+                    self._y -= 1
+                case "right":
+                    self._y += 1
+                case "back":
+                    self._x += 1
+                case "left":
+                    self._y -= 1
 
 
 if __name__ == "__main__":
@@ -169,82 +253,41 @@ if __name__ == "__main__":
     left_wheel_handle = sim.getObjectHandle("/PioneerP3DX/leftMotor")
     right_wheel_handle = sim.getObjectHandle("/PioneerP3DX/rightMotor")
 
-    left = sim.getObjectHandle("/ultrasonicSensor[0]")
-    front = sim.getObjectHandle("/ultrasonicSensor[4]")
-    right = sim.getObjectHandle("/ultrasonicSensor[7]")
-
     base_speed = 2.0
     turn_speed = 0.3
     slow_turn_speed = 0.2
-    more_slow_turn_speed = 0.1
-    MIN_DISTANCE = 0.4
+    slower_turn_speed = 0.1
+    MIN_DISTANCE = 0.5
     kp = 1.0
     angle_tolerance = 0.02  # Tolleranza per considerare l'angolo raggiunto
 
     sim.startSimulation()
     set_speeds(base_speed, base_speed)
 
+    coordinates = CoordinatesDS("front")
+
     try:
         while True:
-            front_dist = get_distance(front)
-            left_dist = get_distance(left)
-            right_dist = get_distance(right)
 
-            front_free = is_free(front)
-            left_free = is_free(left)
-            right_free = is_free(right)
+            coordinates.moving()
+            print(coordinates._x, coordinates._y,
+                  coordinates._direction, coordinates._prev)
+
+            front_free, left_free, right_free = get_free()
 
             if front_free:
                 if not left_free and not right_free:
                     normal_go_straight()
-                elif left_free and not right_free:
-                    if front_dist != 0:
-                        front_dist_new = get_distance(front)
-                        if front_dist_new < front_dist:
-                            print("Giro a sinistra, fottiti")
-                            turn_randomly(False, True, False)
-                        else:
-                            print("WTF")
-                    else:
-                        print("Perciò, sinistra o dritto")
-                        turn_randomly(True, True, False)
-                elif right_free and not left_free:
-                    if front_dist != 0:
-                        front_dist_new = get_distance(front)
-                        if front_dist_new < front_dist:
-                            print("Giro a destra, fottiti")
-                            turn_randomly(False, False, True)
-                        else:
-                            print("WTF")
-                    else:
-                        print("Perciò, destra o dritto")
-                        turn_randomly(True, False, True)
-                elif left_free and right_free:
-                    if front_dist != 0:
-                        front_dist_new = get_distance(front)
-                        if front_dist_new < front_dist:
-                            print("Scelgo tra destra e sinistra")
-                            turn_randomly(False, True, True)
-                        else:
-                            print("WTF")
-                    else:
-                        print("Scelgo tra davanti, sinistra e destra")
-                        turn_randomly(True, True, True)
+                else:
+                    print("Incrocio")
+                    turn_randomly()
             else:
                 if not left_free and not right_free:
-                    print("Vado indietro")
+                    print("Vicolo cieco")
                     go_back()
-                elif right_free and not left_free:
-                    print("Giro a destra")
-                    turn_right()
-                elif left_free and not right_free:
-                    print("Giro a sinistra")
-                    turn_left()
-                elif left_free and right_free:
-                    print("Scelgo tra destra e sinistra")
-                    turn_randomly(False, True, True)
-
-            time.sleep(0.1)
+                else:
+                    print("Incrocio T o curva")
+                    turn_randomly()
 
     finally:
         sim.stopSimulation()
