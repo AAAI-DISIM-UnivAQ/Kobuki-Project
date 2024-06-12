9c2654dc228cb413e5f533842774d0158be6ea30
fab10dl6 <fabiodilazzaro.fdl@gmail.com>
2024-06-12 11:46:11 +0200

    commit

--- New New Project/test_coordinate.py
+++ New New Project/test_coordinate.py
@@ -45,7 +45,7 @@ def go_straight():
     set_speeds(base_speed, base_speed)
 
 
-def turn_randomly():
+def turn_randomly(crossroad):
     time.sleep(1.9)
     coord.change_coordinates(14)
     set_speeds(0, 0)
@@ -58,14 +58,18 @@ def turn_randomly():
         options.append("Left")
     if control_right:
         options.append("Right")
+
     direction = random.choice(options)
 
     if direction == "Right":
         turn_right()
+        crossroad._est = False
     elif direction == "Left":
         turn_left()
+        crossroad._ovest = False
     elif direction == "Front":
         go_straight()
+        crossroad._nord = False
         time.sleep(1.5)
         coord.change_coordinates(11)
 
@@ -138,7 +142,7 @@ def find_target_angle(direction):
             return - math.pi
 
 
-def go_back():
+def go_back(crossroad):
     actual_angle = get_robot_orientation()
     current_angle = normalize_angle(actual_angle)
     target_angle_back = 0
@@ -152,6 +156,7 @@ def go_back():
         target_angle_back = - math.pi/2
     set_robot_orientation(target_angle_back, "front")
     go_straight()
+    crossroad._sud = False
     time.sleep(1.5)
 
 
@@ -180,6 +185,43 @@ class Coordinates:
         self.change_coordinates(1)
 
 
+class Crossroad:
+    _id: int = 0
+    _x: int
+    _y: int
+    _nord: bool
+    _est: bool
+    _sud: bool
+    _ovest: bool
+
+    def __init__(self, x, y, nord, est, sud, ovest):
+        self._id += 1
+        self._x = x
+        self._y = y
+        self._nord = nord
+        self._est = est
+        self._sud = sud
+        self._ovest = ovest
+
+    def choose(self):
+        options = []
+        if self._nord:
+            options.append("nord")
+        if self._est:
+            options.append("est")
+        if self._sud:
+            options.append("sud")
+        if self._ovest:
+            options.append("ovest")
+
+        option = random.choice(options)
+        return option
+
+    def print(self):
+        print("{id:", self._id, "; x:", self._x, "; y:", self._y, "; nord:", self._nord,
+              "; est:", self._est, "; sud:", self._sud, "; ovest:", self._ovest, "}")
+
+
 if __name__ == "__main__":
     client = RemoteAPIClient()
     sim = client.getObject("sim")
@@ -205,21 +247,57 @@ if __name__ == "__main__":
     try:
         while True:
             coord.move()
-            print("X: " + str(coord._x), "Y: " + str(coord._y))
+            # print("X: " + str(coord._x), "Y: " + str(coord._y))
             front_free, left_free, right_free = get_free()
 
             if front_free:
                 if not left_free and not right_free:
                     go_straight()
                 else:
+                    time.sleep(0.1)
                     print("Incrocio")
-                    print("Incroci incontrati:", str(crossroads))
-                    crossroads.append((coord._x, coord._y))
-                    turn_randomly()
+                    print("Incroci incontrati: [")
+
+                    actual_angle = get_robot_orientation()
+                    current_angle = normalize_angle(actual_angle)
+                    if -0.3 < current_angle < 0.3:
+                        print("front")
+                        nord = front_free
+                        est = right_free
+                        sud = False
+                        ovest = left_free
+                    elif -1.8 < current_angle < -1.2:
+                        print("right")
+                        nord = left_free
+                        est = front_free
+                        sud = right_free
+                        ovest = False
+                    elif current_angle < -2.8 or current_angle > 2.8:
+                        print("back")
+                        nord = False
+                        est = left_free
+                        sud = front_free
+                        ovest = right_free
+                    elif 1.2 < current_angle < 1.8:
+                        print("left")
+                        nord = right_free
+                        est = False
+                        sud = left_free
+                        ovest = front_free
+
+                    crossroad = Crossroad(
+                        coord._x, coord._y, nord, est, sud, ovest)
+                    crossroads.append(crossroad)
+
+                    for c in crossroads:
+                        c.print()
+                    print("]")
+
+                    turn_randomly(crossroad)
             else:
                 if not left_free and not right_free:
                     print("Vicolo cieco")
-                    go_back()
+                    go_back(crossroad)
 
     finally:
         sim.stopSimulation()
